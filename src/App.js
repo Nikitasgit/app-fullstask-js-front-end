953fac673d9aa8f55a0c9bc81f222676a37ab40a
Victor Leman <victorleman1@gmail.com>
2023-07-04 20:09:14 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import NewPost from "./components/NewPost";
+import Thread from "./components/Thread";
+import { useDispatch } from "react-redux";
+import { getUser } from "./feature/user.slice";
+
+const App = () => {
+  const [userId, setUserId] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser(userId));
+  }, [userId]);
+  return (
+    <div>
+      <div className="app-container">
+        <div className="login">
+          <h3>Bonjour</h3>
+          <input
+            type="text"
+            placeholder="Pseudo"
+            onChange={(e) => setUserId(e.target.value)}
+          />
+        </div>
+        <NewPost />
+        <Thread />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import LikePost from "./LikePost";
+import axios from "axios";
+import DeletePost from "./DeletePost";
+import { useDispatch, useSelector } from "react-redux";
+import { editPost } from "../feature/post.slice";
+
+const Post = ({ post }) => {
+  const userId = useSelector((state) => state.user.userId);
+  const [isAuthor, setisAuthor] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [newMessage, setNewMessage] = useState("");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (post.author === userId) {
+      setisAuthor(true);
+    } else {
+      setisAuthor(false);
+    }
+  }, [userId]);
+
+  const handleEdit = () => {
+    if (newMessage) {
+      axios.put("http://localhost:5000/post/" + post._id, {
+        message: newMessage,
+      });
+      dispatch(editPost(newMessage, post._id));
+    }
+  };
+  useEffect(() => {}, [userId]);
+  const dateFormater = (date) => {
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+    return new Date(date).toLocaleDateString("fr-FR", options);
+  };
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h3>{post.author}</h3>
+        <p>posté le {dateFormater(post.createdAt)}</p>
+      </div>
+      {isEdit ? (
+        <div className="edit-container">
+          <textarea
+            defaultValue={post.message}
+            onChange={(e) => {
+              setNewMessage(e.target.value);
+            }}
+          ></textarea>
+          <button
+            onClick={() => {
+              setIsEdit(false);
+              handleEdit();
+            }}
+          >
+            Valider édition
+          </button>
+        </div>
+      ) : (
+        <p>{newMessage ? newMessage : post.message}</p>
+      )}
+
+      <div className="icons-part">
+        <LikePost post={post} userId={userId} />
+        {isAuthor && (
+          <div className="update-delete-icons">
+            <span
+              id="update-btn"
+              onClick={() => {
+                setIsEdit(!isEdit);
+                handleEdit();
+              }}
+            >
+              &#10000;
+            </span>
+            <DeletePost postId={post._id} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Post;
